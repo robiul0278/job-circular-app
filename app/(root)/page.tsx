90fa5df779cfc8fb380ce3acaf5fe0fe180fda85
robiul0278 <robiul0278@gmail.com>
2025-07-20 18:22:50 +0600

    update TG channel link

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
           <aside className="lg:col-span-3 space-y-2">
             <div className="rounded-lg p-4 border dark:bg-gray-900">
               <h3 className="font-semibold mb-3 text-sm uppercase">Follow Us</h3>
-              <a href="https://t.me/your_channel_name" target="_blank" rel="noopener noreferrer">
+              <a href="https://t.me/diplomajobsbd" target="_blank" rel="noopener noreferrer">
                 <Button className="flex w-full items-center space-x-2 bg-green-700 hover:bg-green-600 text-white hover:text-white font-medium px-4 py-2 rounded cursor-pointer">
                   <Send size={16} />
                   <span>Telegram</span>

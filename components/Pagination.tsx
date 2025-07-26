fa423a90fc2536e20e3e60c7301236b71b55e6f8
robiul0278 <robiul0278@gmail.com>
2025-07-27 03:11:55 +0600

    update pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,33 +1,40 @@
+'use client';
 
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useTransition } from 'react';
+import { ChevronLeft, ChevronRight, Loader } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import Form from 'next/form';
+import { useRouter } from 'next/navigation';
 
-export default async function PaginationForm({
+export default function PaginationForm({
   currentPage,
   totalPages,
 }: {
   currentPage: number;
   totalPages: number;
 }) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const handlePagination = (page: number) => {
+    startTransition(() => {
+      router.push(`/?page=${page}`);
+    });
+  };
+
   const prevPage = Math.max(1, currentPage - 1);
   const nextPage = Math.min(totalPages, currentPage + 1);
 
   return (
     <div className="flex items-center justify-center gap-4 py-6 text-gray-700 dark:text-gray-300">
-      <Form action="/">
-        <input type="hidden" name="page" value={prevPage} />
-        <Button
-          variant="outline"
-          size="sm"
-          type="submit"
-          disabled={currentPage === 1}
-          className="flex items-center gap-2 cursor-pointer"
-        >
-          <ChevronLeft className="h-4 w-4" />
-          Previous
-        </Button>
-      </Form>
+      <Button
+        variant="outline"
+        size="sm"
+        disabled={currentPage === 1 || isPending}
+        onClick={() => handlePagination(prevPage)}
+        className="flex items-center gap-2"
+      >
+        {isPending ? <Loader/> : <><ChevronLeft className="h-4 w-4" />Previous</>}
+      </Button>
 
       <div className="text-sm">
         Page{' '}
@@ -38,19 +45,15 @@ export default async function PaginationForm({
         <span className="font-medium">{totalPages}</span>
       </div>
 
-      <Form action="/">
-        <input type="hidden" name="page" value={nextPage} />
-        <Button
-          variant="outline"
-          size="sm"
-          type="submit"
-          disabled={currentPage === totalPages}
-          className="flex items-center gap-2 cursor-pointer"
-        >
-          Next
-          <ChevronRight className="h-4 w-4" />
-        </Button>
-      </Form>
+      <Button
+        variant="outline"
+        size="sm"
+        disabled={currentPage === totalPages || isPending}
+        onClick={() => handlePagination(nextPage)}
+        className="flex items-center gap-2"
+      >
+        {isPending ? <Loader/> : <>Next<ChevronRight className="h-4 w-4" /></>}
+      </Button>
     </div>
   );
 }

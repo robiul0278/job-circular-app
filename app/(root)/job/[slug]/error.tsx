d65b4674ad8ece118d77c346a09205181a78318c
robiul0278 <robiul0278@gmail.com>
2025-07-30 00:45:19 +0600

    UPDATE error page

--- app/(root)/job/[slug]/error.tsx
+++ app/(root)/job/[slug]/error.tsx
@@ -10,7 +10,7 @@ export default function Error({
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[70vh] px-4 text-center">
-      <h1 className="text-3xl font-bold text-red-600">😥 কিছু ভুল হয়েছে</h1>
+      <h1 className="text-3xl font-bold text-red-600">😥 কিছু ভুল হয়েছে [JOBS]</h1>
       <p className="mt-3 text-green-600">
         {error.message}
       </p>

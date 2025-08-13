cb4fbcc24483119ad00dbd63fae31e7b9bb18f32
robiul0278 <robiul0278@gmail.com>
2025-08-13 16:32:41 +0600

    update more jobs

--- components/ShowMoreJobs.tsx
+++ components/ShowMoreJobs.tsx
@@ -13,26 +13,26 @@ export default async function ShowMoreJobs() {
 
     return (
         <div className="border shadow p-4 rounded-lg">
-            <h2 className="text-lg font-bold mb-4 text-slate-700 dark:text-slate-200">
+            <h2 className="text-lg font-bold mb-3 text-slate-700 dark:text-slate-200">
                 সরকারি চাকরির বিজ্ঞপ্তি
             </h2>
 
             {result.length > 0 ? (
-                <ul className="space-y-3">
+                <ul className="space-y-2">
                     {result.map((job: IJobCircular) => (
                         <li
                             key={job._id}
-                                 className="border-b border-gray-200 dark:border-gray-700 pb-2 last:border-none"
+                            className="border-b border-gray-200 dark:border-gray-700 pb-2 last:border-none"
                         >
                             <Link
                                 href={`/job/${job.slug}`}
                                 className="block group"
                                 aria-label={`View details for ${job.title}`}
                             >
-                                <h3 className="font-semibold group-hover:text-green-800 dark:group-hover:text-green-400">
+                                <h3 className="text-sm font-medium group-hover:text-green-800 dark:group-hover:text-green-400">
                                     {job.title}
                                 </h3>
-                                <span className="text-sm text-green-800">
+                                <span className="text-xs text-green-800 dark:text-green-600">
                                     শেষ তারিখঃ <strong>{formatDate(job.deadline)}</strong>
                                 </span>
                             </Link>
@@ -40,8 +40,8 @@ export default async function ShowMoreJobs() {
                     ))}
                 </ul>
             ) : (
-                <p className="text-center text-gray-500 dark:text-gray-400">
-                    কোনো সরকারি চাকরির বিজ্ঞপ্তি পাওয়া যায়নি।
+                <p className="text-center text-gray-500 dark:text-gray-400 text-sm">
+                    বিজ্ঞপ্তি পাওয়া যায়নি।
                 </p>
             )}
         </div>

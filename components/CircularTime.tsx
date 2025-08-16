4c9b39f7806e373f78b72bdae3ba8c06b0dd406a
robiul0278 <robiul0278@gmail.com>
2025-08-16 20:13:18 +0600

    fix: refine layout and spacing in CircularTime component for improved readability

--- components/CircularTime.tsx
+++ components/CircularTime.tsx
@@ -10,26 +10,30 @@ import { deadlineCountdown, formatDate } from "@/utils/format-date";
 
 export default async function JobCircularTimeTable({deadline}:{deadline: string}) {
     return (
-        <div className="mt-4">
-            <Table>
-                <TableBody className="">
-                    <TableRow>
-                        <TableCell className="flex items-center space-x-1">
-                            <Clock4 className="w-4 h-4" />
-                            <span >আবেদনের শেষ তারিখঃ</span>
-                        </TableCell>
-                        <TableCell className="text-right">{formatDate(deadline)}</TableCell>
-                    </TableRow>
+<div className="">
+  <Table>
+    <TableBody>
+      <TableRow>
+        <TableCell className="flex items-center space-x-1 px-0 py-1">
+          <Clock4 className="w-4 h-4" />
+          <span>আবেদনের শেষ তারিখঃ</span>
+        </TableCell>
+        <TableCell className="text-right px-0 py-1">{formatDate(deadline)}</TableCell>
+      </TableRow>
 
-                    <TableRow>
-                        <TableCell className="flex items-center space-x-1">
-                            <Hourglass className="w-4 h-4 animate-pulse" />
-                            <span >আবেদন শেষ হতেঃ</span>
-                        </TableCell>
-                        <TableCell className="text-right text-amber-600">{deadlineCountdown(deadline)}</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </div>
+      <TableRow>
+        <TableCell className="flex items-center space-x-1 px-0 py-1">
+          <Hourglass className="w-4 h-4 animate-pulse" />
+          <span>আবেদন শেষ হতেঃ</span>
+        </TableCell>
+        <TableCell className="text-right text-amber-600 px-0 py-1">
+          {deadlineCountdown(deadline)}
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</div>
+
+
     );
 }
